9cae6e2966efcdf754756a7b6e95e40cad847a3e
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-07-28 00:12:38 +0800

    Finished the rest of the tool stack section

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiNextdotjs, SiSolidity } from "react-icons/si";
-import { FaPlaceOfWorship, FaRust } from "react-icons/fa";
 import Go from "../../Assets/TechIcons/go.svg";
 import C from "../../Assets/TechIcons/C++.svg";
 import Javascript from "../../Assets/TechIcons/Javascript.svg";
@@ -29,50 +27,73 @@ import CSS from "../../Assets/TechIcons/CSS.svg";
 import PHP from "../../Assets/TechIcons/PHP.svg";
 import Spring from "../../Assets/TechIcons/Spring.svg";
 import Flutter from "../../Assets/TechIcons/Flutter.svg";
-
-
+import { 
+  SiCplusplus,
+  SiHtml5,
+  SiCss3,
+  SiFlutter,
+  SiPhp,
+  SiSpringboot,
+  SiLaravel,
+  SiSvelte,
+  SiSqlite,
+  SiVite,
+  SiChartdotjs,
+  SiNextdotjs,
+  SiTableau,
+  SiGooglecloud
+} from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <img src={C} alt="C++" />
+        <SiCplusplus alt="C++" />
         <div className="tech-icons-text">C++</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Javascript} alt="javascript" />
         <div className="tech-icons-text">Javascript</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Typescript} alt="typescript" />
         <div className="tech-icons-text">Typescript</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={HTML} alt="typescript" className="new-icons"/>
+        <SiHtml5 alt="typescript"/>
         <div className="tech-icons-text">HTML</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={CSS} alt="typescript" className="new-icons"/>
+        <SiCss3 alt="css"/>
         <div className="tech-icons-text">CSS</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Node} alt="node" />
         <div className="tech-icons-text">Node.Js</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={ReactIcon} alt="react" />
         <div className="tech-icons-text">React.Js</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={FaPlaceOfWorship} alt="php" className="new-icons"/>
+        <SiPhp alt="php"/>
         <div className="tech-icons-text">PHP</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Spring} alt="mongoDb" className="new-icons"/>
+        <SiSpringboot alt="spring-boot"/>
         <div className="tech-icons-text">Spring Boot</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Redux} alt="redux" />
+        <SiLaravel alt="laravel" />
         <div className="tech-icons-text">Laravel</div>
       </Col>
 
@@ -80,29 +101,34 @@ function Techstack() {
         <SiNextdotjs fontSize={"24px"} />
         <div className="tech-icons-text">Next.js</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Git} alt="git" />
         <div className="tech-icons-text">Git</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Firebase} alt="firebase" />
         <div className="tech-icons-text">Firebase</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Redis} alt="redis" />
+        <SiSvelte alt="redis" />
         <div className="tech-icons-text">Svelte</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <img src={Docker} alt="docker" />
         <div className="tech-icons-text">Docker</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Kubernates} alt="kubernetes" />
+        <SiSqlite alt="SQLite" />
         <div className="tech-icons-text">SQLite</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <img src={SQL} alt="SQL" />
+        <img src={SQL} alt="PostgreSQL" />
         <div className="tech-icons-text">Postgresql</div>
       </Col>
 
@@ -110,12 +136,14 @@ function Techstack() {
         <img src={Python} alt="Python" />
         <div className="tech-icons-text">Python</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Java} alt="haskell" />
+        <img src={Java} alt="Java" />
         <div className="tech-icons-text">Java</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <img src={HaskellIcon} alt="haskell" />
+        <SiTableau alt="haskell" />
         <div className="tech-icons-text">Tableau</div>
       </Col>
 
@@ -135,18 +163,28 @@ function Techstack() {
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <FaRust fontSize={"24px"} />
+        <SiVite alt="vite" />
         <div className="tech-icons-text">Vite</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <img src={AWS} alt="Postman" className="tech-icon-images" />
+        <img src={AWS} alt="AWS" className="tech-icon-images" />
         <div className="tech-icons-text">AWS</div>
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiChartdotjs alt="Chart.Js" />
+        <div className="tech-icons-text">Chart.Js</div>
+      </Col>
+
+       <Col xs={4} md={2} className="tech-icons">
+        <SiFlutter alt="flutter" />
+        <div className="tech-icons-text">Flutter</div>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <img src={Kafka} alt="Chart.Js" className="tech-icon-images" />
-        <div className="tech-icons-text">Chart.Js</div>
+        <SiGooglecloud alt="google-cloud" />
+        <div className="tech-icons-text">Google Cloud</div>
       </Col>
     </Row>
   );
