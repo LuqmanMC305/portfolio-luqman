897aeaf5ffcbb4055d0c71c0cffdf0a465bc2fb7
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-07-29 00:06:52 +0800

    feat: added my projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,11 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import monitus from "../../Assets/Projects/monitus.png";
+import usmmarketplace4u from "../../Assets/Projects/usmmarketplace4u.png";
+import campusconnect from "../../Assets/Projects/campusconnect.png";
+
+
 
 function Projects() {
   return (
@@ -15,42 +20,42 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Notable <strong className="purple">Projects </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are some projects I already worked on.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={monitus}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Monitus"
+              description="Localised emergency alert platform with geofencing, real-time notifications and an admin dashboard, using Flutter, Laravel, PostgreSQL, and Firebase Cloud Messaging (FCM). Awarded the Bronze Award at the PIXEL 2026 Final Year Project Competition."
+              ghLink="https://github.com/LuqmanMC305/monitus-project"
+              //demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={campusconnect}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Campus Connect"
+              description="Interactive web platform that centralises USM campus events using a map interface. Students can browse, filter and submit events for administrator approval. It was part of one of the university group assignment. The used tech stack are Vite, TypeScript, and Firebase Cloud Storage."
+              ghLink="https://github.com/Raxatal/CampusConnect-Py4"
+              //demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={usmmarketplace4u}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="USM Marketplace4U"
+              description="Student recommerce platform for buying and selling second-hand items, powered by Next.Js and Firebase Cloud Storage."
+              ghLink="https://github.com/LuqmanMC305/usmmarketplace4u"
+              //demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
@@ -58,8 +63,8 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              title="ANPR Smart Parking System"
+              description="AI-powered parking management system using YOLOv8 and OCR for real-time licence plate recognition. Includes payment dashboard and Flask API. Achieved Technology Readiness Level (TRL) 4 during university startup accelerator testing."
               ghLink="https://github.com/soumyajit4419/Plant_AI"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
@@ -69,9 +74,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Hangman Game"
+              description="A text-based hangman game coded in C++."
+              ghLink="https://github.com/LuqmanMC305/hangman-game"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -80,9 +85,8 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              title="My Portfolio Website"
+              description="The designed is based on soumyajit4419. The original work can be found on https://github.com/soumyajit4419/Portfolio"
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
