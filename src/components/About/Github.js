0d4b3846e40c657c109680b2b68b8ddb62c7fb3a
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-08-05 18:52:29 +0800

    fix: made github calendar readable for mobile devices

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {useState, useEffect} from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
 function Github() {
+
+  const [isMobile, setIsMobile] = useState (window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+     // Listen for browser resize events
+    window.addEventListener("resize", handleResize);
+
+     // Remove the listener when the component is removed
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Row
       style={{
@@ -14,13 +32,13 @@ function Github() {
       <h1 className="project-heading pb-4" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
-        username="luqmanmc305"
-        blockSize={30}
-        blockMargin={10}
-        color="#c084f5"
-        fontSize={20}
-      />
+        <GitHubCalendar
+          username="luqmanmc305"
+          blockSize={isMobile ? 10 : 15}
+          blockMargin={isMobile ? 3 : 5}
+          color="#c084f5"
+          fontSize={isMobile ? 10 : 14}
+        />
     </Row>
   );
 }
