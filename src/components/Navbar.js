37dea7a93a0092c8c2b72d49411e6276ec1ce43a
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-08-05 18:28:02 +0800

    fix: changed the fork button link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -103,7 +103,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/LuqmanMC305/portfolio-luqman"
                 target="_blank"
                 className="fork-btn-inner"
               >
